6381a065cd4d702fc53497384eb53440105a0505
juliaodynak <julia.odynak@gmail.com>
2015-09-03 12:08:35 +0300

    Initial Commit

---
+++ calc_pol/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  calc_pol
+//
+//  Created by adminaccount on 9/3/15.
+//  Copyright (c) 2015 pelekh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ calc_pol/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  calc_pol
+//
+//  Created by adminaccount on 9/3/15.
+//  Copyright (c) 2015 pelekh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
